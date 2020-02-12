b66a6209637507fb577a67a17844e23aca9cd3d7
xwen <1769694815@qq.com>
2020-02-12 13:41:28 +0800

    设个vuex的getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,11 @@
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name,
+  access_token: state => state.user.access_token,
+  refresh_token: state => state.user.refresh_token,
+  expires_in: state => state.user.expires_in,
+  permissions: state => state.user.permissions,
+  userInfo: state => state.user.userInfo,
   roles: state => state.user.roles,
   permission_routes: state => state.permission.routes
 }
